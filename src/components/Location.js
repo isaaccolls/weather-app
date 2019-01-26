6f19eaccc1c653fdc114df8660122fca6312224d
Isaac Colls <colls_isaac@hotmail.com>
2019-01-25 23:41:12 -0300

    adding more prop types

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = (props) => {
     // console.log(props);
@@ -12,6 +13,10 @@ const Location = (props) => {
     return (
         <div><h1>{city}</h1></div>
     );
+};
+
+Location.propTypes = {
+    city: PropTypes.string.isRequired,
 };
 
 export default Location;
