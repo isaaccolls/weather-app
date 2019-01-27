b6a5c16cc46b74589ab91e0eb4686537b770962b
Isaac Colls <colls_isaac@hotmail.com>
2019-01-27 20:15:11 -0300

    styles for location and weatherLocation

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = (props) => {
     // console.log(props);
@@ -11,7 +12,11 @@ const Location = (props) => {
     const { city } = props;
 
     return (
-        <div><h1>{city}</h1></div>
+        <div className="locationCont">
+            <h1>
+                {city}
+            </h1>
+        </div>
     );
 };
 
