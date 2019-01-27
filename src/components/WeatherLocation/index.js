32a68d0ad63b753cf7278df076f08d33ccd529eb
Isaac Colls <colls_isaac@hotmail.com>
2019-01-27 20:55:40 -0300

    weatherLocation from Functional component to Class component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
@@ -13,11 +13,25 @@ const data = {
     wind: "10 m/s",
 };
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={'Barcelona'}></Location>
-        <WeatherData data={ data }></WeatherData>
-    </div>
-);
+// const WeatherLocation = () => (
+//     <div className="weatherLocationCont">
+//         <Location city={'Barcelona'}></Location>
+//         <WeatherData data={ data }></WeatherData>
+//     </div>
+// );
+
+class WeatherLocation extends Component {
+
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={'Barcelona'}></Location>
+                <WeatherData data={ data }></WeatherData>
+            </div>
+        );
+    }
+
+};
+    
 
 export default WeatherLocation;
