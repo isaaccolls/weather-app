56d56f6d82d6fcc62b690366bbc61fc78eb1c875
Isaac Colls <colls_isaac@hotmail.com>
2019-01-27 19:19:24 -0300

    adding some css

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons'; 
 import PropTypes from 'prop-types';
+import './styles.css';
 import {
     CLOUD,
     CLOUDY,
@@ -32,7 +33,7 @@ const getWeatherIcon = weatherState => {
 };
 
 const WeatherTemperature = ( { temperature , weatherState }) => (//21, "snow"
-    <div>
+    <div className="weatherTemperatureCont">
         { getWeatherIcon(weatherState) }
         <span>{ `${temperature}°C` }</span>
     </div>
--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -4,9 +4,10 @@ import WeatherTemperature from './WeatherTemperature';
 import {
     SNOW,//"snow"
 } from "../../../constants/weather";
+import './styles.css';
 
 const WeatherData = () => (
-    <div>
+    <div className="weatherDataCont">
         <WeatherTemperature temperature={ 21 } weatherState={ SNOW }>
         </WeatherTemperature>
         <WeatherExtraInfo humidity={ 88 } wind={ "10 m/s" }>
