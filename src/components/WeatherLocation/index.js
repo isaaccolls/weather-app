94184e5b9910d2163dfe1c61a3ef3927aac4e142
Isaac Colls <colls_isaac@hotmail.com>
2019-01-27 21:11:20 -0300

    using onClick and state on weatherLocation

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -13,6 +13,13 @@ const data = {
     wind: "10 m/s",
 };
 
+const data1 = {
+    temperature: 15,
+    weatherState: SNOW,
+    humidity: 73,
+    wind: "200 m/s",
+};
+
 // const WeatherLocation = () => (
 //     <div className="weatherLocationCont">
 //         <Location city={'Barcelona'}></Location>
@@ -22,11 +29,29 @@ const data = {
 
 class WeatherLocation extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data: data,
+        };
+    }
+
+    handleUpdateCick = () => {
+        console.log("actualizado");
+        this.setState({
+            city: 'Colorado',
+            data: data1,
+        });
+    }
+
     render() {
+        const { city, data } = this.state;
         return (
             <div className="weatherLocationCont">
-                <Location city={'Barcelona'}></Location>
+                <Location city={ city }></Location>
                 <WeatherData data={ data }></WeatherData>
+                <button onClick={ this.handleUpdateCick }>Actualizar</button>
             </div>
         );
     }
