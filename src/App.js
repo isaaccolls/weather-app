73530dca25debc5e0e0907a554c04550b773eeab
Isaac Colls <colls_isaac@hotmail.com>
2019-01-29 14:14:48 -0300

    passing location list as a parameter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+    'Buenos Aires,ar',
+    'Bogota,col',
+    'Mexico,mex',
+    'Washington,us',
+    'Barcelona,es',
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LocationList></LocationList>
+        <LocationList cities={ cities }></LocationList>
       </div>
     );
   }
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import WeatherLocation from './WeatherLocation';
 
-const LocationList = () => (
-    <div>
-        <WeatherLocation city="Buenos Aires,ar" />
-        <WeatherLocation city="Bogota,col" />
-        <WeatherLocation city="Mexico,mex" />
-        <WeatherLocation city="Washington,us" />
-        <WeatherLocation city="Barcelona,es" />
-    </div>
-);
+const LocationList = ({ cities }) => {
+    console.log(cities);
+    return (
+        <div>
+            <WeatherLocation city="Buenos Aires,ar" />
+            <WeatherLocation city="Bogota,col" />
+            <WeatherLocation city="Mexico,mex" />
+            <WeatherLocation city="Washington,us" />
+            <WeatherLocation city="Barcelona,es" />
+        </div>
+    );
+};
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired,
+};
 
 export default LocationList;
