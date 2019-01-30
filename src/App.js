b13526652017e8eb5182002f3a075ee566630bd5
Isaac Colls <colls_isaac@hotmail.com>
2019-01-30 13:47:08 -0300

    adding the Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 import './App.css';
@@ -21,7 +25,13 @@ class App extends Component {
     return (
       <Grid>
         <Row>
-          Titulo
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
         </Row>
         <Row>
           <Col xs={12} md={6}>
