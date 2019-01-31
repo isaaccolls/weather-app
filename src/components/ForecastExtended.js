76082b958ce4945c80ad90bba5f2cc300c233c24
Isaac Colls <colls_isaac@hotmail.com>
2019-01-31 14:28:09 -0300

    passing array of days to ForecastItem

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,16 +3,29 @@ import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css';
 
+const days = [
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+];
+
 // always remember!!
 // const city = this.props.city;
 // const { city } = city;
 class ForecastExtended extends Component {
+
+    renderForecastItemDays() {
+        return days.map(day => (<ForecastItem weekDay={day}></ForecastItem>));
+    }
+
     render() {
         const { city } = this.props;
         return (
             <div>
                 <h2 className="forecast-title">Pronostico Extendido {city}</h2>
-                <ForecastItem></ForecastItem>
+                {this.renderForecastItemDays()}
             </div>
         );
     }
--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ForecastItem = () => (
-    <div>Item</div>
+const ForecastItem = ({ weekDay }) => (
+    <div>{weekDay}</div>
 );
+
+ForecastItem.propsTypes = {
+    weekDay: PropTypes.string.isRequired,
+};
 
 export default ForecastItem;
