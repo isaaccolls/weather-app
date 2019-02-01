c685a559da1c775d557369576382b993f009af8d
Isaac Colls <colls_isaac@hotmail.com>
2019-02-01 00:25:38 -0300

    refactoring to fetch data for ForecastExtended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,36 +3,53 @@ import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css';
 
-const days = [
-    'lunes',
-    'martes',
-    'miercoles',
-    'jueves',
-    'viernes',
-];
+// const days = [
+//     'lunes',
+//     'martes',
+//     'miercoles',
+//     'jueves',
+//     'viernes',
+// ];
 
-const data = {
-    temperature: 21,
-    humidity: 20,
-    weatherState: 'normal',
-    wind: 'normal'
-};
+// const data = {
+//     temperature: 21,
+//     humidity: 20,
+//     weatherState: 'normal',
+//     wind: 'normal'
+// };
 
 // always remember!!
 // const city = this.props.city;
 // const { city } = city;
 class ForecastExtended extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            forecastData: null,
+        };
+    }
+
     renderForecastItemDays() {
-        return days.map(day => (<ForecastItem weekDay={day} hour={420} data={data}></ForecastItem>));
+        return "Render Items";
+        // return days.map(day => (<ForecastItem weekDay={day} hour={420} data={data}></ForecastItem>));
+    }
+
+    renderProgess() {
+        return <h3>Cargando pronostico extendido</h3>;
     }
 
     render() {
         const { city } = this.props;
+        const { forecastData } = this.state;
         return (
             <div>
                 <h2 className="forecast-title">Pronostico Extendido {city}</h2>
-                {this.renderForecastItemDays()}
+                {
+                    forecastData ?
+                    this.renderForecastItemDays() :
+                    this.renderProgess()
+                }
             </div>
         );
     }
