43325eaf70775e83b4495bde78a24da3b8f5ce50
Isaac Colls <colls_isaac@hotmail.com>
2019-02-06 01:41:45 -0300

    refactor with reselect

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { createSelector } from 'reselect';
 import { cities, getForecastDataFromCities as _getForecastDataFromCities } from './cities';
 import { city } from './city';
 
@@ -9,6 +10,8 @@ export default combineReducers({
     city,
 });
 
-export const getCity = state => state.city;
+// export const getCity = state => state.city;
+export const getCity = createSelector(state => state.city, city => city);
 
-export const getForecastDataFromCities = state => (_getForecastDataFromCities(state.cities, getCity(state)));
+// export const getForecastDataFromCities = state => (_getForecastDataFromCities(state.cities, getCity(state)));
+export const getForecastDataFromCities = createSelector(state => state.cities, getCity, _getForecastDataFromCities);
